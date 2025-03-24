c8da09c60033b234ac7d8b3619a2383101ccf09c
moauiz-09 <rlqqyn@gmail.com>
2025-03-24 16:44:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//  wii zin 
+const btn_nav= document.getElementById('btn-01');
+const section_nav =document.getElementById('part');
+
+
+  btn_nav.onclick =()=>{
+ section_nav.classList.toggle('h')
+
+  }
+
